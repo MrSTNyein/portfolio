da230c38b59399915c2302555b590cd4abb097ca
SITHU NYEIN <sithunyein.official@hotmail.com>
2025-10-03 13:21:46 +0630

    sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
-const CACHE_NAME = 'portfolio-cache-v1';
+// Define a name for the cache
+const CACHE_NAME = 'sithu-nyein-portfolio-v1';
+
+// List all the files to be cached
 const urlsToCache = [
   '/',
   '/index.html',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
-  'https://i.ibb.co/fVZ8JR2j/IMG-3199.jpg' 
-  // You can add more key assets here
+  '/style.css',
+  '/script.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap'
+  // Note: We don't cache your profile image from ibb.co as it's an external resource.
+  // Caching external resources can be complex. We are focusing on the core app shell.
 ];
 
-// Install the service worker and cache assets
+// Install event: triggered when the service worker is first installed
 self.addEventListener('install', event => {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -18,16 +24,36 @@ self.addEventListener('install', event => {
   );
 });
 
-// Serve cached assets when offline
+// Fetch event: triggered for every network request made by the page
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
+        // Cache hit - return response from cache
         if (response) {
           return response;
         }
+
+        // Not in cache - fetch from network
         return fetch(event.request);
-      })
+      }
+    )
+  );
+});
+
+// Activate event: triggered when the service worker is activated
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
